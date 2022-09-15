a075ae0d7fad0149218224883902332168c08793
Kiyrean Dyer-Allen <kda_100@yahoo.co.uk>
2022-09-15 09:49:38 +0100

    more service tests

--- src/test/java/com/Sakila/api/SakilaApp/Category/CategoryServiceTest.java
+++ src/test/java/com/Sakila/api/SakilaApp/Category/CategoryServiceTest.java
@@ -7,7 +7,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
@@ -17,14 +21,22 @@ class CategoryServiceTest {
 
     private CategoryService underTest;
 
+    private List<Category> testCategories;
+
     @BeforeEach
     void setUp() {
+        testCategories = new ArrayList<Category>();
+        for(Long i = 0l; i < 10; i++) {
+            testCategories.add(new Category(i, "name"));
+        }
         underTest = new CategoryService(categoryRepository);
     }
 
     @Test
     void getAllCategories() {
-        underTest.getCategories();
+        doReturn(testCategories).when(categoryRepository).findAll();
+        List<Category> categories = underTest.getCategories();
         verify(categoryRepository).findAll();
+        assertEquals(testCategories, categories);
     }
 }
