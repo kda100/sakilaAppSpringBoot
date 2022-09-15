eccf08688efcf26304442f194d4007b3e28678fd
Kiyrean Dyer-Allen <kda_100@yahoo.co.uk>
2022-09-15 17:20:50 +0100

    more tests added

---
+++ src/test/java/com/Sakila/api/SakilaApp/Category/CategoryTest.java
@@ -0,0 +1,47 @@
+package com.Sakila.api.SakilaApp.Category;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.util.Assert.isInstanceOf;
+
+class CategoryTest {
+    Long categoryId = 1l;
+    String categoryName = "Horror";
+    private Category category;
+
+    @BeforeEach
+    void setup() {
+        category = new Category(categoryId, categoryName);
+    }
+
+    @Test
+    void getId() {
+        assertEquals(categoryId, category.getId());
+    }
+
+    @Test
+    void getName() {
+        assertEquals(categoryName, category.getName());
+    }
+
+    @Test
+    void setId() {
+        Long newId = 10l;
+        category.setId(newId);
+        assertEquals(newId, category.getId());
+    }
+
+    @Test
+    void setName() {
+        String newCategoryName = "Comedy";
+        category.setName(newCategoryName);
+        assertEquals(newCategoryName, category.getName());
+    }
+
+    @Test
+    void testToString() {
+        isInstanceOf(String.class, category.toString());
+    }
+}
--- src/test/java/com/Sakila/api/SakilaApp/Country/CountryDTOTest.java
+++ src/test/java/com/Sakila/api/SakilaApp/Country/CountryDTOTest.java
@@ -1,32 +1,48 @@
 package com.Sakila.api.SakilaApp.Country;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.util.Assert.isInstanceOf;
 
 class CountryDTOTest {
 
+    String countryName = "United Kingdom";
+    private CountryDTO countryDTO;
+
+
+    @BeforeEach
+    void setUp() {
+        countryDTO = new CountryDTO(countryName);
+    }
+
     @Test
     void getCountry() {
+        assertEquals(countryName, countryDTO.getCountry());
     }
 
     @Test
     void setCountry() {
+        String newCountry = "France";
+        countryDTO.setCountry(newCountry);
+        assertEquals(newCountry, countryDTO.getCountry());
     }
 
     @Test
     void testEquals() {
+        assertEquals(true, countryDTO.equals(countryDTO));
     }
 
-    @Test
-    void canEqual() {
-    }
 
     @Test
     void testHashCode() {
+        isInstanceOf(Integer.class, countryDTO.hashCode());
     }
 
     @Test
     void testToString() {
+        isInstanceOf(String.class, countryDTO.toString());
     }
 }
